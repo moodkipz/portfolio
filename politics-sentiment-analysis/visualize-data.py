a5f323a4b5891271807735319678074d8023114c
moodkipz <xprussias@gmail.com>
2023-04-19 10:49:19 -0400

    Switching to a horizontal bar chart

--- politics-sentiment-analysis/visualize-data.py
+++ politics-sentiment-analysis/visualize-data.py
@@ -5,26 +5,26 @@ import sqlite3
 # Connect to the database
 conn = sqlite3.connect('politicsdata.sqlite')
 
-# read the threads table
+# Read the threads table
 threads_df = pd.read_sql_query("SELECT id AS thread_id, thread_title, title_sentiment FROM Threads", conn)
 
-# read the comments table
+# Read the comments table
 comments_df = pd.read_sql_query("SELECT thread_id, comment_sentiment FROM comments", conn)
 
-# join the two tables on thread_id
+# Join the two tables on thread_id
 joined_df = pd.merge(threads_df, comments_df, on='thread_id')
 
-# select required columns
+# Select required columns
 final_df = joined_df[['thread_title', 'title_sentiment', 'comment_sentiment']]
 
-# group by thread_title and calculate mean sentiment
+# Group by thread_title and calculate mean sentiment
 grouped_df = final_df.groupby('thread_title').agg({'title_sentiment': 'mean', 'comment_sentiment': 'mean'})
 
-# plot the sentiment values
+# Plot the sentiment values
 fig, ax = plt.subplots(figsize=(10,8))
-grouped_df.plot(kind='bar', ax=ax)
-ax.set_xlabel('Thread Title')
-ax.set_ylabel('Sentiment')
+grouped_df.plot(kind='barh', ax=ax)
+ax.set_xlabel('Sentiment')
+ax.set_ylabel('Thread Title')
 ax.set_title('Thread Sentiment and Overall Comment Sentiment')
 plt.show()
 
